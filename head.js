fac2373b085524a9b0d508e3a967fdb8897f5f2c
Camillia Snow <s.camillia@gmail.com>
2023-08-27 12:19:27 -0300

    adding new head function to grab and return first element in an array

---
+++ head.js
@@ -0,0 +1,20 @@
+// initial function to determine if the assertion will pass or fail //
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// adding a function to return the first element in the array //
+const head = function(array) {
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["cat", "dog"]),"cat");
+// additional tests to try for failures //
+assertEqual(head([""]), undefined);
+assertEqual(head([]), "taco");
+assertEqual(head(["taco"]), undefined);
