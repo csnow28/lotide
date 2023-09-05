f077e029910606b6f5b77f5d2a99afc8e2144151
Camillia Snow <s.camillia@gmail.com>
2023-09-05 15:03:17 -0300

    adding our own version of .map() with assertion checks

---
+++ map.js
@@ -0,0 +1,31 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+// added const for empty arrays in order to properly compare and utilize eqArrays messaging //
+const actual = [];
+const expected = [];
+if (eqArrays(actual, expected)) {
+  console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+} else if (actual !== expected) {
+  console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+//const words = ["Halloween", "Cats", "Bats", "Witches", "Pumpkins"];
+const words = ["purple", "blue", 5];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+const results1 = map(words, word => word[0]);
+console.log(results1);
