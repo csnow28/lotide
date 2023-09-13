fe78e96aa00148a8c2dc2fc00426ce3c14b1c5da
Camillia Snow <s.camillia@gmail.com>
2023-09-13 20:58:57 -0300

    Adding description of function, code has been linted.

--- tail.js
+++ tail.js
@@ -1,4 +1,7 @@
-// initial function to determine if the assertion will pass or fail //
+// this function (tail) takes in an array, and returns all elements in the array
+// except the first element in the array
+
+// initial function to determine if the assertion will pass or fail
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
@@ -6,12 +9,12 @@ const assertEqual = function(actual, expected) {
     console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-// function to return all but first element in the array //
+// function to return all but first element in the array
 const tail = function(array) {
   return array.slice(1);
 };
 
-// test to ensure original array is not modified with code //
+// test to ensure original array is not modified with code
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
 assertEqual(words.length, 3);
