42b4443b2255552cfb29871db490b86423117373
Camillia Snow <s.camillia@gmail.com>
2023-09-13 20:55:56 -0300

    Adding the actual description of the function, fully linted

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,12 @@
+// create a function, assertEqual, that takes in two values (actual, expected).
+// add assertions via console.log, that returns true if the values are equal, otherwise false
+// used emojis to help make the assertion checks more visible
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🟢 Assertion Passed: " + actual + " === " + expected);
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log("🔴 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
