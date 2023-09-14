b74262d1d032f68f719c55d5552b9d7eee1ba9f7
Camillia Snow <s.camillia@gmail.com>
2023-09-13 21:33:34 -0300

    adding description of function, adding another test case, linted

--- without.js
+++ without.js
@@ -1,4 +1,4 @@
-const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
@@ -11,15 +11,16 @@ const eqArrays = function (array1, array2) {
 };
 
 const assertEqual = function(actual, expected) {
-//const actual = [];
-//const expected = [];
-if (eqArrays(actual, expected)) {
-  console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-} else if (actual !== expected) {
-  console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-}
-}; 
+  if (eqArrays(actual, expected)) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
+// function that will take an elements in an array and will return only the elements from the source array
+// that are not in the itemsToRemove array.
+// The function values are source, and itemsToRemove
 
 const without = function(source, itemsToRemove) {
   //add an empty results array to return a new array without the unwanted items
@@ -27,25 +28,26 @@ const without = function(source, itemsToRemove) {
   // time to loop over the items in the source string
   for (let i = 0; i < source.length; i++) {
   //add a value that is false to continue the loop
-  let toRemove = false;
-  //add a second loop over itemsToRemove array to check if an item here matches an item in the source array
+    let toRemove = false;
+    //add a second loop over itemsToRemove array to check if an item here matches an item in the source array
     for (let w = 0; w < itemsToRemove.length; w++) {
       if (source[i] === itemsToRemove[w])
       //if an item does match, the toRemove then becomes true
-      toRemove = true;
+        toRemove = true;
     }
-  //once the loops are finished, time to take the items to remove (if true!) and push 
-  if (!toRemove) {
-    results.push(source[i]);
-  }
+    //once the loops are finished, time to take the items to remove (if true!) and push
+    if (!toRemove) {
+      results.push(source[i]);
+    }
   }
   return results;
 };
 
 console.log(without([1, 2, 3], [1]));
 console.log(without(["1", "2", "3"], [1, 2, "3"]));
+console.log(without(["taco", "cat", "is", "boo", "cool"], ["boo"]));
 
 //const words = ["hello", "world", "lighthouse"];
 //without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
-//assertArraysEqual(words, ["hello", "world", "lighthouse"]); 
+//assertEqual(eqArrays(words, ["hello", "world", "lighthouse"]));
