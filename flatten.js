ce38fe367beb18b9cc75e35b46f70a34b43b7bf3
Camillia Snow <s.camillia@gmail.com>
2023-09-13 21:40:44 -0300

    adding description of function, adding a test code, fixing comments and linted.

--- flatten.js
+++ flatten.js
@@ -1,4 +1,4 @@
-const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
@@ -16,26 +16,30 @@ if (eqArrays(actual, expected)) {
   console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
 } else if (actual !== expected) {
   console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-};
+}
 
-const flatten = function (arrayBag) {
-  //add empty array for result
-const result = [];
-// loop through the array
+// creating a function with the value of arrayBag (tempted to rename it to halloweenBag...), which takes the array
+// containing elements, and even nested elements, and then returns a 'flattened' version, so one singular array.
+
+const flatten = function(arrayBag) {
+  // add empty array for result
+  const result = [];
+  // loop through the array
   for (let i = 0; i < arrayBag.length; i++) {
-    //check to see if there are arrays nested
+    // check to see if there are any nested arrays
     if (Array.isArray(arrayBag[i])) {
-      //loop through nested array elements
+      // loop through nested array elements
       for (let j = 0; j < arrayBag[i].length; j++) {
-        //push the nested array elements into new array, 3, 4, 6
+        // push the nested array elements into new array (in test code: 3, 4, 6)
         result.push(arrayBag[i][j]);
       }
     } else {
-      //push into the array if it isn't an array already, in the test that would be 1, 2, 5
+      // push into the array if it isn't an array already (test code: 1, 2, 5)
       result.push(arrayBag[i]);
     }
   }
-    return result;
+  return result;
 };
 
 console.log(flatten([1, 2, [3, 4], 5, [6]]));
+console.log(flatten([["chips"], ["skittles", "sour patch kids", "swedish berries", "kit kat"], ["suckers"]]));
