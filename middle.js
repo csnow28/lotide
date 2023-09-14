ec32cf4f68077cbf7c26bd6681a9d9c45166e2a9
Camillia Snow <s.camillia@gmail.com>
2023-09-13 21:52:29 -0300

    added function description, added and fixed comments, linted.

--- middle.js
+++ middle.js
@@ -1,4 +1,4 @@
-/* const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
@@ -9,29 +9,34 @@
   }
   return true;
 };
-// added const for empty arrays in order to properly compare and utilize eqArrays messaging //
-const assertEqual = function(actual, expected) {
-//const actual = [];
-//const expected = [];
-if (eqArrays(actual, expected)) {
-  console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-} else if (actual !== expected) {
-  console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-}
-}; */
 
+const assertEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// function middle takes in the value of array, and will return the middle
+// element of the array. If uneven, will take middle two, and will not
+// return if the array contains only one or two elements
 
 const middle = function(array) {
   const length = array.length;
   const midIndex = Math.floor(length / 2);
 
+  // add in the check if the array length is less or equal to 2, in which case
+  // return an empty array
   if (length <= 2) {
     return [];
+    // if length of the array is even, return the two middle values
   } else if (length % 2 === 0) {
     const middle1 = array[midIndex - 1];
     const middle2 = array[midIndex];
     return [middle1, middle2];
   } else {
+    // if the array length is odd, perfect! return the middle element.
     return [array[midIndex]];
   }
 };
