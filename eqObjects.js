73b67a948e540b217c21befcfd72711e4ae13b86
Camillia Snow <s.camillia@gmail.com>
2023-09-13 22:13:31 -0300

    fixed the description of the function, kept comments and linted.

--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,4 @@
-const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
@@ -17,28 +17,30 @@ const assertEqual = function(actual, expected) {
     console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-//testing the return value of functions via objects
+
+// function that will compare two objects and will return true if they are equal
+// and will return false if not.
 
 const eqObjects = function(object1, object2) {
   //check the keys of the objects using the Object.keys
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
-// check to see if the number of keys is the same
-if (keys1.length !== keys2.length) {
-  return false;
-}
-//check to see if the values of each key are the same
+  // check to see if the number of keys is the same
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  //check to see if the values of each key are the same
   for (const key of keys1) {
     if (object1[key] !== object2[key]) {
       return false;
     }
   }
-return true;
+  return true;
 };
 
 const multiColorShirtObject = { colors: ["red", "blue",], size: "medium" };
 const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
 assertEqual(eqObjects(multiColorShirtObject , anotherMultiColorShirtObject), true);
 
-const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
 assertEqual(eqObjects(multiColorShirtObject , longSleeveMultiColorShirtObject), false);
