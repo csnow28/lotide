08ebec9ef5999b45e634e7511d0cf57cfbf2b300
Camillia Snow <s.camillia@gmail.com>
2023-09-15 20:30:39 -0300

    added in mocha and chai, with new test code.

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,11 @@
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["cat", "dog"]),"cat");
-// additional tests to try for failures //
-assertEqual(head([""]), undefined);
-assertEqual(head([]), "taco");
-assertEqual(head(["taco"]), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
