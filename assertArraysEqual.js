0fd0ba490b7a482b61aed1cb480b1b3db11f3039
Camillia Snow <s.camillia@gmail.com>
2023-09-15 20:22:36 -0300

    fixed code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,12 @@
 // adding in a new function that will compare two arrays, and using the eqArrays functions, will return
 // either a pass or fail as a check.
-const eqArrays = require("../eqArrays")
+const eqArrays = require("./eqArrays")
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`🟢 Assertion Passed: ${array1} === ${array2}`);
   } else if (array1 !== array2) {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🔴 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
 
