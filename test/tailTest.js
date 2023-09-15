b451a85976ae6eafabae7c5ae3169367456d7170
Camillia Snow <s.camillia@gmail.com>
2023-09-15 20:42:18 -0300

    adding mocha and chai with new test code

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,15 @@
 const tail = require("../tail")
+const assert = require("chai").assert
 
 // tests to ensure original array is not modified with code via tail function
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns '[Lighthouse, Labs]' for [Yo Yo, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), [])
+  });
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), [])
+  });
+})
