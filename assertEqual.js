2e18afa9c1eaf8e7c542dd8f04ddf6106dfdeb9a
Camillia Snow <s.camillia@gmail.com>
2023-09-15 16:17:53 -0300

    Refactored: test cases now put into own file.

--- assertEqual.js
+++ assertEqual.js
@@ -1,18 +1,15 @@
-// create a function, assertEqual, that takes in two values (actual, expected).
-// add assertions via console.log, that returns true if the values are equal, otherwise false
-// used emojis to help make the assertion checks more visible
+// create a function, assertEqual, that takes in two values (actual, expected) that returns true if the values are equal, otherwise false
+// use of emojis help visualize the assertions quickly
 
 const assertEqual = function(actual, expected) {
+  // checks if actual equals/the same as expected
   if (actual === expected) {
+    // logs a pass if they are equal/the same
     console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+    // logs a fail if they are not equal/the same
   } else if (actual !== expected) {
     console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual(3,5);
-assertEqual("Camillia", "Snow");
-assertEqual(1,"cat");
-assertEqual("taco", "taco");
+module.exports = assertEqual;
