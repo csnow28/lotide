f57c641024aaffcb753b9b0888a0acfa9b963d34
Camillia Snow <s.camillia@gmail.com>
2023-09-15 20:04:19 -0300

    Refactored: added test code from function.

---
+++ test/middleTest.js
@@ -0,0 +1,10 @@
+const middle = require("../middle")
+
+// adding test cases for middle function
+
+console.log(middle([1]));
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
