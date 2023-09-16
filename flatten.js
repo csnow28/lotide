7d74b8f642bb4b1d05ddc88d04c3d1b374c4f5b4
Camillia Snow <s.camillia@gmail.com>
2023-09-16 13:15:31 -0300

    Refactored: test code put into own file, cleaned up comments.

--- flatten.js
+++ flatten.js
@@ -1,23 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-// added const for empty arrays in order to properly compare and utilize eqArrays messaging //
-const actual = [];
-const expected = [];
-if (eqArrays(actual, expected)) {
-  console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-} else if (actual !== expected) {
-  console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-}
-
 // creating a function with the value of arrayBag (tempted to rename it to halloweenBag...), which takes the array
 // containing elements, and even nested elements, and then returns a 'flattened' version, so one singular array.
 
@@ -41,5 +21,4 @@ const flatten = function(arrayBag) {
   return result;
 };
 
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
-console.log(flatten([["chips"], ["skittles", "sour patch kids", "swedish berries", "kit kat"], ["suckers"]]));
+module.exports = flatten
