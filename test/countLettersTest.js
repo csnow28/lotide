2cc332a6c3b552a6bca50efc90534d244d3abd25
Camillia Snow <s.camillia@gmail.com>
2023-09-16 13:24:11 -0300

    Refactored: test code from function, cleaned up test.

---
+++ test/countLettersTest.js
@@ -0,0 +1,9 @@
+const countLetters = require("../countLetters")
+const assertEqual = require("../assertEqual")
+
+// test code
+
+assertEqual(countLetters("lighthouse in the house"));
+assertEqual(countLetters("Halloween is awesome"));
+assertEqual(countLetters("Pumpkins are brilliant  "));
+assertEqual(countLetters("  "));
