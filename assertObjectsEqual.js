04861dfaa010c778908768aece790084d20f7f38
Camillia Snow <s.camillia@gmail.com>
2023-09-16 13:38:34 -0300

    Updated code, refactored but not happy. Will continue to refactor. Test code moved into own file.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,27 +1,11 @@
-const eqObjects = function(object1, object2) {
-  //check the keys of the objects using the Object.keys
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-  // check to see if the number of keys is the same
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-  //check to see if the values of each key are the same
-  for (const key of keys1) {
-    if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqObjects = require("../eqObjects")
 
-// cleaning up and adding in the visual assertions to eqObjects
-
-const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
-  if (eqObjects(actual, expected)) {
-    console.log(`🟢 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+const assertObjectsEqual = function (object1, object2) {
+  if (eqObjects(object1, object2)) {
+    console.log(`🟢🟢 Assertion Passed: ${object1} === ${object2}`);
   } else {
-    console.log(`🔴 Assertion Failed: ${inspect(actual)} === ${inspect(expected)}`);
+    console.log(`🔴🔴 Assertion Failed: ${object1} !== ${object2}`);
   }
 };
+
+module.exports = assertObjectsEqual
