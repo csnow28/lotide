598bfc4f52c3020c54a186272f77a07a08c87ce6
Camillia Snow <s.camillia@gmail.com>
2023-09-16 13:44:28 -0300

    Refactored, test code added from function.

---
+++ test/findKeyTest.js
@@ -0,0 +1,27 @@
+const assertEqual = require("../assertEqual")
+const findKey = require("../findKey")
+
+// test code
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2));
+
+const ratings = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}
+const result = findKey(ratings, x => x.stars === 2)
+console.log(result)
+
+assertEqual(result, "noma");
+assertEqual(result, "Akelarre");
