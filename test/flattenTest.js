4f06a7e7482b1d43748f4c31014387b6a6d52919
Camillia Snow <s.camillia@gmail.com>
2023-09-16 13:13:24 -0300

    Refactored: added test code from function.

---
+++ test/flattenTest.js
@@ -0,0 +1,6 @@
+const flatten = require("../flatten")
+
+// test code
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
+console.log(flatten([["chips"], ["skittles", "sour patch kids", "swedish berries", "kit kat"], ["suckers"]]));
