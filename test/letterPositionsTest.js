69f7a955d32c615e1f9e3468ab973c5b0f56f87c
Camillia Snow <s.camillia@gmail.com>
2023-09-16 13:17:28 -0300

    Refactored: added test code for function.

---
+++ test/letterPositionsTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = rewquire("../assertArraysEqual")
+const letterPositions = require("../letterPositions")
+
+// test code
+
+console.log(letterPositions("Halloween"));
+console.log(letterPositions("poetry for neanderthals"));
+console.log(letterPositions("Momo is a cool cat"));
