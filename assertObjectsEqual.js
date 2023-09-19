c3f40adce2eb50eb5d900f87a46d5b62bfb08b6b
Camillia Snow <s.camillia@gmail.com>
2023-09-19 17:45:55 -0300

    Adding inspect require

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,6 +1,7 @@
 const eqObjects = require("./eqObjects")
 
 const assertObjectsEqual = function (object1, object2) {
+  const inspect = require('util').inspect;
   if (eqObjects(object1, object2)) {
     console.log(`🟢🟢 Assertion Passed: ${object1} === ${object2}`);
   } else {
