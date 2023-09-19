75d33842dbf6b79e9b9c32550bb3fdd5be6713fd
Camillia Snow <s.camillia@gmail.com>
2023-09-19 18:20:03 -0300

    fixing code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -5,7 +5,7 @@ const eqArrays = require("./eqArrays")
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`🟢 Assertion Passed: ${array1} === ${array2}`);
-  } else if (array1 !== array2) {
+  } else {
     console.log(`🔴 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
