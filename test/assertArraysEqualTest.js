dd08540aab2355030692720a4f852b4b1d21c664
Camillia Snow <s.camillia@gmail.com>
2023-09-19 17:54:39 -0300

    removing a require

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,6 +1,4 @@
 const assertArraysEqual = require("../assertArraysEqual")
-const eqArrays= require("../eqArrays")
-
 // added some quick test cases
 eqArrays(assertArraysEqual([5, 6, 7, 8], [5, 6, 7, 8])); // => true
 eqArrays(assertArraysEqual([0, 9, 8], [8, 9, 0])); // => false
